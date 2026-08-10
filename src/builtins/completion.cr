55a0b05e64779d10dd3494bb43be2faa2a91270c
Sebastien Dunne <sebastien.dunne@oxaro.com>
2026-08-10 11:53:00 -0400

    include forwards and actions in completions

--- src/builtins/completion.cr
+++ src/builtins/completion.cr
@@ -4,6 +4,14 @@ module Builtins
       BUILTINS.each_key do |c|
         puts c
       end
+
+      @ops_yml.forwards.each_key do |c|
+        puts c
+      end
+
+      @ops_yml.actions.each_key do |c|
+        puts c
+      end
       true
     end
   end
